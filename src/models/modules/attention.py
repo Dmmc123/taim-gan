204abaef40659f31f98c25d2a62632d9f994bc40
Dmmc123 <r.mihaylov@innopolis.university>
2022-10-09 16:56:21 +0400

    TG-20 replace conv2d with conv1d in attn

--- src/models/modules/attention.py
+++ src/models/modules/attention.py
@@ -4,7 +4,7 @@ from typing import Any
 import torch
 from torch import nn
 
-from src.models.modules.conv_utils import conv1d, conv2d
+from src.models.modules.conv_utils import conv1d
 
 
 class ChannelWiseAttention(nn.Module):
@@ -21,12 +21,9 @@ class ChannelWiseAttention(nn.Module):
         """
         super().__init__()
         # perception layer
-        self.text_conv = conv2d(text_d, fm_size, kernel_size=1)
+        self.text_conv = conv1d(text_d, fm_size)
         # attention across channel dimension
         self.softmax = nn.Softmax(1)
-        # dimensionalities of output feature maps
-        self.fm_size = fm_size
-        self.text_d = text_d
 
     def forward(self, v_k: torch.Tensor, w_text: torch.Tensor) -> Any:
         """
@@ -37,13 +34,7 @@ class ChannelWiseAttention(nn.Module):
         :return: Fused hidden visual features and word features
         :rtype: Any
         """
-        # get sqrt of L from input
-        emb_l = w_text.size(2)
-        # convert embs to 2d feature maps
-        w_text = w_text.view(-1, self.text_d, emb_l, 1)
         w_hat = self.text_conv(w_text)
-        # return to flat embs
-        w_hat = w_hat.view(-1, self.fm_size, emb_l)
         m_k = v_k @ w_hat
         a_k = self.softmax(m_k)
         w_hat = torch.transpose(w_hat, 1, 2)
@@ -62,7 +53,7 @@ class SpatialAttention(nn.Module):
         """
         super().__init__()
         self.softmax = nn.Softmax(1)
-        self.conv = conv1d(d, d_hat, kernel_size=1)
+        self.conv = conv1d(d, d_hat)
 
     def forward(self, text_context: torch.Tensor, image: torch.Tensor) -> Any:
         """
