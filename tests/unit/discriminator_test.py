122ad584d431077113a3f9dedd2e440e54987ae5
Dmmc123 <r.mihaylov@innopolis.university>
2022-10-24 19:02:05 +0400

    TG-24 set chans=3 in discriminator

--- tests/unit/discriminator_test.py
+++ tests/unit/discriminator_test.py
@@ -6,20 +6,18 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    argnames=("batch_num", "chans", "height", "width", "num_words"),
+    argnames=("batch_num", "height", "width", "num_words"),
     argvalues=(
-        (64, 3,  14, 14, 1),
-        (32, 2,  20, 20, 2),
-        (16, 1,  17, 14, 4),
-        (8,  1,  32, 32, 8),
-        (4,  5,  16, 16, 16),
-        (2,  10, 13, 18, 32),
-        (1,  7,  13, 26, 64),
+        (64, 14, 14, 1),
+        (32, 7,  2,  3),
+        (16, 3,  7,  7),
     )
 )
-def test_d(batch_num, chans, height, width, num_words):
-    v = torch.rand((batch_num, chans, height, width))
-    w = torch.rand((batch_num, chans, num_words))
-    D = Discriminator(img_chans=chans)
+def test_d(batch_num, height, width, num_words):
+    # generate fake rgb images
+    v = torch.rand((batch_num, 3, height, width))
+    # generate contextual embeddings of the same channels
+    w = torch.rand((batch_num, 3, num_words))
+    D = Discriminator(emb_len=3)
     logits = D(v, w)
-    assert logits.size(0) == batch_num and logits.size(1) == num_words, "output dims should never change"
+    assert logits.size() == (batch_num, num_words), "output dims should never change"
